17b3be16babbc201ecf8bb2d7c269d3d44a4e08a
javina-lmbl <jorge.avina@logicamobile.com>
2023-01-10 11:30:41 -0600

    chore: Images and format manifest

--- lmmx/__manifest__.py
+++ lmmx/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Logica Mobile México',
+    'version': '15.0.1.0.0',
+    'summary': 'Logica Mobile México module for internal processes',
+    'website': 'https://www.logicamobilemexico.mx',
     'description': 'Logica Mobile México module for internal processes',
     'author': 'LMMX',
     'application': True,
     'license': "AGPL-3",
-    'website': 'https://www.logicamobilemexico.mx',
     'category': "Services/LmmX",
-    'version': '15.0.1.0.0',
     'depends': [
         'base',
         'stock',
