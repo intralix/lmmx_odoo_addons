eea722128b3a3729700f837e3e38c506d627608c
javina-lmbl <jorge.avina@logicamobile.com>
2023-01-30 11:44:18 -0600

    chore: force update

--- lmmx/__manifest__.py
+++ lmmx/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Logica Mobile México',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'summary': 'Logica Mobile México internal processes',
     'sequence': 50,
     'website': 'https://www.logicamobilemexico.mx',
