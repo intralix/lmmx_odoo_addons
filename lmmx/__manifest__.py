b26b0815292b08f33388450d7042189fa0cbc10d
javina-lmbl <jorge.avina@logicamobile.com>
2023-01-30 10:49:59 -0600

    chore: format init file

--- lmmx/__manifest__.py
+++ lmmx/__manifest__.py
@@ -2,16 +2,30 @@
 {
     'name': 'Logica Mobile México',
     'version': '15.0.1.0.0',
-    'summary': 'Logica Mobile México module for internal processes',
+    'summary': 'Logica Mobile México internal processes',
+    'sequence': 50,
     'website': 'https://www.logicamobilemexico.mx',
-    'description': 'Logica Mobile México module for internal processes',
+    'description': """
+Logica Mobile Mexico
+====================
+This module provides to the company features for work with internal processes 
+
+This module is maintained by internal developers.
+    """,
     'author': 'LMMX',
+    'installable': True,
     'application': True,
-    'license': "AGPL-3",
+    'auto_install': False,
     'category': "Services/Lmmx",
     'depends': [
         'base',
         'mail',
+        'stock',
+        'contacts',
+        'account',
+        'sale_subscription',
+        'helpdesk',
+        'repair',
     ],
     'data': [
         'security/security.xml',
@@ -19,4 +33,5 @@
         'views/main_menu.xml',
         'views/cell_chip.xml',
     ],
+    'license': "AGPL-3",
 }
