7cbd2155a7d113e4affadd22cd08212e44c1b751
javina <code@intralix.com>
2024-05-14 16:00:12 -0600

    migrate to v17

--- lmmx/__manifest__.py
+++ lmmx/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Logica Mobile México',
-    'version': '15.0.1.0.1',
+    'version': '17.0.1.0.1',
     'summary': 'Logica Mobile México internal processes',
     'sequence': 50,
     'website': 'https://www.logicamobilemexico.mx',
@@ -29,17 +29,14 @@ This module is maintained by internal developers.
     ],
     'data': [
         'security/security.xml',
-        'security/ir.model.access.csv',
-        #'data/ir_cron_data.xml',
+        'security/ir.model.access.csv',        
         'views/main_menu.xml',
         'views/cell_chip.xml',
         'views/custom_employee.xml',
         'views/custom_partner.xml',
         'views/custom_repair.xml',
-        'views/custom_stocking.xml',
-        'views/custom_invoice_report.xml',
-        #'views/custom_account_move.xml',
-        #'views/res_config_settings_views.xml',
+        #'views/custom_stocking.xml',
+        'views/custom_invoice_report.xml',        
     ],
     'license': "AGPL-3",
 }
